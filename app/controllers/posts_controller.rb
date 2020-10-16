3eb776844251cf889bd1f7b11ec1e03bb884e089
F-tomo <tmk.determination714@icloud.com>
2020-10-16 22:55:05 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,8 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content], checked: false)
-    redirect Json:{ post: post }
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
 
   def checked
